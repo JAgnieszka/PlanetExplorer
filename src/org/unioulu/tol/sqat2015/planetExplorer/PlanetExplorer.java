9792aa4a80450df2df260276c58ea11d9d32ee16
somename <someemail>
2015-10-23 11:49:49 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -38,9 +38,10 @@ public class PlanetExplorer {
 
 	public String gridSize() {
 		String xx=Integer.toString(grid.length); //get x and convert to string
-		String yy=Integer.toString(grid.length); //get x and convert to string
+		String yy=Integer.toString(grid[0].length); //get x and convert to string
 		
 		System.out.println("xx "+xx);
+		System.out.println("yy "+yy);
 		return null;
 	}
 }
