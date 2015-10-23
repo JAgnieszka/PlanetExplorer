3a9f6bb8b09536a4ae61dda96d862fc0ccb07a13
somename <someemail>
2015-10-23 12:48:21 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -28,9 +28,9 @@ public class PlanetExplorer {
 		System.out.println("obstacles "+obstacles);
 		if(obstacles != ""){
 			String obsX = obstacles.substring(1,2);
-			System.out.println("obsX "+obsX);
+//			System.out.println("obsX "+obsX);
 			String obsY = obstacles.substring(3,4);
-			System.out.println("obsY "+obsY);
+//			System.out.println("obsY "+obsY);
 			
 		}else{
 			directionDefoultEX="north";
